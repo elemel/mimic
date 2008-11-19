ae64221644a8b41e4f1012f83851facbd2a4d757
Mikael Lind <elemel@elemel.se>
2008-11-19 21:28:33 +0100

    Added MIT License file. Updated the README file. Imported implementation and unit test from the Moonshine project.

---
+++ src/mimic.lua
@@ -0,0 +1,340 @@
+-- Copyright (c) 2008 Mikael Lind
+-- 
+-- Permission is hereby granted, free of charge, to any person
+-- obtaining a copy of this software and associated documentation
+-- files (the "Software"), to deal in the Software without
+-- restriction, including without limitation the rights to use,
+-- copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the
+-- Software is furnished to do so, subject to the following
+-- conditions:
+-- 
+-- The above copyright notice and this permission notice shall be
+-- included in all copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+-- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+-- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+-- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+-- HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+-- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+-- OTHER DEALINGS IN THE SOFTWARE.
+
+local function identity(...)
+    return ...
+end
+
+local function iter_str(s)
+    local index = 0
+    local length = #s
+    return function()
+        index = index + 1
+        if index <= length then
+            return string.sub(s, index, index)
+        else
+            return nil
+        end
+    end
+end
+
+local function values(t)
+    local key, value = nil, nil
+    return function()
+        key, value = next(t, key)
+        return value
+    end
+end
+
+local function iter(iterable)
+    if type(iterable) == "function" then
+        return iterable
+    elseif type(iterable) == "string" then
+        return iter_str(iterable)
+    elseif type(iterable) == "table" then
+        local iter_func = iterable["__iter"]
+        if iter_func ~= nil then
+            return iter_func(iterable)
+        else
+            return values(iterable)
+        end
+    else
+        error("argument is not iterable")
+    end
+end
+
+local function dict(iterable)
+    local result = {}
+    for key, value in iter(iterable) do
+        result[key] = value
+    end
+    return result
+end
+
+local function reduce(iterable, func, init)
+    local next_item = iter(iterable)
+    local result = init
+    if result == nil then
+        result = next_item()
+        if result == nil then
+            return nil
+        end
+    end
+    for item in next_item do
+        result = func(result, item)
+    end
+    return result
+end
+
+local function add(left, right)
+    return left + right
+end
+
+local function all(iterable, pred)
+    pred = pred or identity
+    local next_item = iter(iterable)
+    for item in next_item do
+        if not pred(item) then
+            return false
+        end
+    end
+    return true
+end
+
+local function any(iterable, pred)
+    pred = pred or identity
+    local next_item = iter(iterable)
+    for item in next_item do
+        if pred(item) then
+            return true
+        end
+    end
+    return false
+end
+
+local function list(iterable)
+    local result = {}
+    for item in iter(iterable) do
+        table.insert(result, item)
+    end
+    return result
+end
+
+local function blank(value)
+    if type(value) == "nil" then
+        return true
+    elseif type(value) == "boolean" then
+        return value == false
+    elseif type(value) == "number" then
+        return value == 0
+    elseif type(value) == "string" then
+        return value == ""
+    elseif type(value) == "table" then
+        local blank_func = value["__blank"]
+        if blank_func ~= nil then
+            return blank_func(value)
+        else
+            return next(value) == nil
+        end
+    else
+        return false
+    end
+end
+
+local function enum(iterable)
+    local index = 0
+    local next_item = iter(iterable)
+    return function()
+        item = next_item()
+        if item ~= nil then
+            index = index + 1
+            return index, item
+        else
+            return nil
+        end
+    end
+end
+
+local function equiv(tab_1, tab_2)
+    for key, value in pairs(tab_1) do
+        if value ~= tab_2[key] then
+            return false
+        end
+    end
+    for key, value in pairs(tab_2) do
+        if value ~= tab_1[key] then
+            return false
+        end
+    end
+    return true
+end
+
+local function filter(iterable, pred)
+    local next_item = iter(iterable)
+    return function()
+        local item = next_item()
+        if item ~= nil and pred(item) then
+            return item
+        else
+            return nil
+        end
+    end
+end
+
+local function items(t)
+    local key, value = nil, nil
+    return function()
+        key, value = next(t, key)
+        return key, value
+    end
+end
+
+local function join(iterable, separator)
+    return table.concat(list(iterable), (separator or " "))
+end
+
+local function keys(t)
+    local key, value = nil, nil
+    return function()
+        key, value = next(t, key)
+        return key
+    end
+end
+
+local function map(iterable, func)
+    local next_item = iter(iterable)
+    return function()
+        local item = next_item()
+        if item == nil then
+            return nil
+        else
+            return func(item)
+        end
+    end
+end
+
+local function mul(left, right)
+    return left * right
+end
+
+local function product(iterable)
+    return reduce(iterable, mul, 1)
+end
+
+local function range(first, last, step)
+    first = first or 1
+    step = step or 1
+    first = first - step
+    return function()
+        first = first + step
+        if last == nil or step > 0 and first <= last or
+           step < 0 and first >= last then
+            return first
+        else
+            return nil
+        end
+    end
+end
+
+local function repr(value)
+    if type(value) == "string" then
+        return "\"" .. string.gsub(value, "\"", "\\\"") .. "\""
+    elseif type(value) == "table" then
+        result = "{"
+        first = true
+        for key, value in pairs(value) do
+            if first then
+                first = false
+            else
+                result = result .. ", "
+            end
+            result = result .. "[" .. repr(key) .. "]" .. " = " .. repr(value)
+        end
+        result = result .. "}"
+        return result
+    else
+        return tostring(value)
+    end
+end
+
+local function set(iterable)
+    local result = {}
+    for item in iter(iterable) do
+        result[item] = true
+    end
+    return result
+end
+
+local function split(s, pattern, plain)
+    pattern = pattern or "[ \t\r\n]+"
+    local first, last = 1, 0
+    local length = #s
+    return function()
+        if first > length then
+            return nil
+        end
+        local init = last + 1
+        first, last = string.find(s, pattern, init, plain)
+        if first == nil then
+            first, last = length + 1, length
+        end
+        return string.sub(s, init, first - 1)
+    end
+end
+
+local function sum(iterable)
+    return reduce(iterable, add, 0)
+end
+
+local function zip_all(iterable_1, iterable_2)
+    local next_item_1 = iter(iterable_1)
+    local next_item_2 = iter(iterable_2)
+    return function()
+        local item1 = next_item_1()
+        local item2 = next_item_2()
+        if item_1 ~= nil and item_2 ~= nil then
+            return item_1, item_2
+        else
+            return nil
+        end
+    end
+end
+
+local function zip_any(iterable_1, iterable_2)
+    local next_item_1 = iter(iterable_1)
+    local next_item_2 = iter(iterable_2)
+    return function()
+        local item1 = next_item_1()
+        local item2 = next_item_2()
+        if item_1 ~= nil or item_2 ~= nil then
+            return item_1, item_2
+        else
+            return nil
+        end
+    end
+end
+
+return {
+    all = all,
+    any = any,
+    blank = blank,
+    enum = enum,
+    equiv = equiv,
+    dict = dict,
+    filter = filter,
+    items = items,
+    iter = iter,
+    join = join,
+    keys = keys,
+    list = list,
+    map = map,
+    product = product,
+    range = range,
+    repr = repr,
+    set = set,
+    split = split,
+    sum = sum,
+    values = values,
+    zip_all = zip_all,
+    zip_any = zip_any,
+}
---
+++ src/mimic_test.lua
@@ -0,0 +1,146 @@
+-- Copyright (c) 2008 Mikael Lind
+-- 
+-- Permission is hereby granted, free of charge, to any person
+-- obtaining a copy of this software and associated documentation
+-- files (the "Software"), to deal in the Software without
+-- restriction, including without limitation the rights to use,
+-- copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the
+-- Software is furnished to do so, subject to the following
+-- conditions:
+-- 
+-- The above copyright notice and this permission notice shall be
+-- included in all copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+-- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+-- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+-- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+-- HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+-- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+-- OTHER DEALINGS IN THE SOFTWARE.
+
+local mimic = require "mimic"
+
+local function assert_false(value)
+    assert(not value)
+end
+
+local function assert_equiv(value1, value2)
+    assert(mimic.equiv(value1, value2))
+end
+
+local function assert_not_equiv(value1, value2)
+    assert(not mimic.equiv(value1, value2))
+end
+
+local function test_all()
+    assert(mimic.all({}))
+    assert(mimic.all({true}))
+    assert(mimic.all({true, true, true}))
+    assert_false(mimic.all({false}))
+    assert_false(mimic.all({false, true, true}))
+    assert_false(mimic.all({true, false, true}))
+    assert_false(mimic.all({true, true, false}))
+    assert_false(mimic.all({false, false, false}))
+end
+
+local function test_any()
+    assert_false(mimic.any({}))
+    assert(mimic.any({true}))
+    assert(mimic.any({true, true, true}))
+    assert_false(mimic.any({false}))
+    assert(mimic.any({false, true, true}))
+    assert(mimic.any({true, false, true}))
+    assert(mimic.any({true, true, false}))
+    assert_false(mimic.any({false, false, false}))
+end
+
+local function test_blank()
+    assert(mimic.blank(nil))
+    assert(mimic.blank(false))
+    assert(mimic.blank(0))
+    assert(mimic.blank(""))
+    assert(mimic.blank({}))
+    
+    assert_false(mimic.blank(true))
+    assert_false(mimic.blank(1))
+    assert_false(mimic.blank("a"))
+    assert_false(mimic.blank({1}))
+    assert_false(mimic.blank({a = 1}))
+end
+
+local function test_equiv()
+    assert_equiv({1, 2, 3}, {1, 2, 3})
+    assert_not_equiv({1, 2, 3}, {1, 2, 4})
+    assert_not_equiv({1, 2, 3, 4}, {1, 2, 3})
+    assert_not_equiv({1, 2, 3}, {1, 2, 3, 4})
+end
+
+local function test_join()
+    assert(mimic.join({"a", "b", "c"}) == "a b c")
+end
+
+local function test_list()
+    assert_equiv(mimic.list(mimic.range(1, 3)), {1, 2, 3})
+    assert_equiv(mimic.list("Lua"), {"L", "u", "a"})
+end
+
+local function test_product()
+    assert(mimic.product({1, 2, 3, 4, 5, 6}) == 720)
+end
+
+local function test_range()
+    i = mimic.range()
+    assert(i() == 1)
+    assert(i() == 2)
+    assert(i() == 3)
+
+    i = mimic.range(5, 7)
+    assert(i() == 5)
+    assert(i() == 6)
+    assert(i() == 7)
+    assert(i() == nil)
+end
+
+local function test_repr()
+    assert(mimic.repr(nil) == "nil")
+    assert(mimic.repr(false) == "false")
+    assert(mimic.repr(true) == "true")
+    assert(mimic.repr(0) == "0")
+    assert(mimic.repr(123456789) == "123456789")
+    assert(mimic.repr("") == "\"\"")
+    assert(mimic.repr("a") == "\"a\"")
+    assert(mimic.repr("\"") == "\"\\\"\"")
+    assert(mimic.repr({}) == "{}")
+    assert(mimic.repr({13}) == "{[1] = 13}")
+    assert(mimic.repr({a = 13}) == "{[\"a\"] = 13}")
+    assert(mimic.repr({"a", "b", "c"}) ==
+                      "{[1] = \"a\", [2] = \"b\", [3] = \"c\"}")
+end
+
+local function test_split()
+    assert_equiv(mimic.list(mimic.split("The Dude abides.")),
+                 {"The", "Dude", "abides."})
+end
+
+local function test_sum()
+    assert(mimic.sum({1, 2, 3, 4, 5, 6}) == 21)
+end
+
+local function test()
+    test_all()
+    test_any()
+    test_blank()
+    test_equiv()
+    test_join()
+    test_list()
+    test_product()
+    test_range()
+    test_repr()
+    test_split()
+    test_sum()
+end
+
+test()
